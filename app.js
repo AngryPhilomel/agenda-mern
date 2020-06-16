b36db65fbec2d2e8c84c640be3ea8384cff83247
AngryPhilomel <an.aries@icloud.com>
2020-06-16 10:50:27 +0300

    deploy ready

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const authRouter = require('./routes/auth.routes')
 const calendarRouter = require('./routes/calendar.routes')
@@ -8,9 +9,16 @@ const calendarRouter = require('./routes/calendar.routes')
 const app = express()
 
 app.use(express.json({extended:true}))
-
 app.use('/api/auth', authRouter)
 app.use('/api/calendar', calendarRouter)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+    app.get('*', (req, res) => {
+       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')) 
+    })
+}
 
 const PORT = config.get('port') || 5000
 
